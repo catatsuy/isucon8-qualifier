9e31ba5544ff39aad929acd32918e9016300acd7
catatsuy <catatsuy@catatsuy.org>
2018-09-15 10:34:42 +0900

    仮デプロイ

---
+++ webapp/go/deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -x
+
+./deploy_body.sh | notify_slack
---
+++ webapp/go/deploy_body.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+
+echo "start deploy ${USER}"
+GOOS=linux go build -o torb src/torb/app.go
+for server in isu01 isu02 isu03; do
+  # ssh -t $server "sudo systemctl stop torb.go"
+  # scp ./torb $server:/home/isucon/torb/webapp/go/.
+  # rsync -av ./views/ $server:/home/isucon/torb/webapp/go/views/
+  # ssh -t $server "sudo systemctl start torb.go"
+done
+
+echo "finish deploy ${USER}"
