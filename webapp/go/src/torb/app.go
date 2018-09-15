5cef002dda7c3d0b51f65b7104c7077c02c48558
Tatsuhiko Kubo <cubicdaiya@gmail.com>
2018-09-15 13:35:21 +0900

    redude sql issue for sheets.

--- webapp/go/src/torb/app.go
+++ webapp/go/src/torb/app.go
@@ -210,6 +210,21 @@ func getEvents(all bool) ([]*Event, error) {
 		events = append(events, &event)
 	}
 
+
+	rows, err = db.Query("SELECT * FROM sheets ORDER BY `rank`, num")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var sheets []Sheet
+	for rows.Next() {
+		var sheet Sheet
+		if err := rows.Scan(&sheet.ID, &sheet.Rank, &sheet.Num, &sheet.Price); err != nil {
+			return nil, err
+		}
+		sheets = append(sheets, sheet)
+	}
+
 	for i, event := range events {
 		event.Sheets = map[string]*Sheets{
 			"S": &Sheets{},
@@ -217,12 +232,6 @@ func getEvents(all bool) ([]*Event, error) {
 			"B": &Sheets{},
 			"C": &Sheets{},
 		}
-
-		rows, err := db.Query("SELECT * FROM sheets ORDER BY `rank`, num")
-		if err != nil {
-			return nil, err
-		}
-		defer rows.Close()
 
 		rRows, err := db.Query("SELECT * FROM reservations WHERE event_id = ? AND canceled_at IS NULL", event.ID)
 		if err != nil {
@@ -243,11 +252,7 @@ func getEvents(all bool) ([]*Event, error) {
 			}
 		}
 
-		for rows.Next() {
-			var sheet Sheet
-			if err := rows.Scan(&sheet.ID, &sheet.Rank, &sheet.Num, &sheet.Price); err != nil {
-				return nil, err
-			}
+		for _, sheet := range sheets {
 			event.Sheets[sheet.Rank].Price = event.Price + sheet.Price
 			event.Total++
 			event.Sheets[sheet.Rank].Total++
